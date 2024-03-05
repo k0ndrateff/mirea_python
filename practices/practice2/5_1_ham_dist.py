54ba0b347f5abb85c85bdabc3681ad2adfd3ba6d
Egor Kondratev <kondratevegor04@gmail.com>
2024-03-05 15:39:08 +0300

    fix: 5.1

--- practices/practice2/5_1_ham_dist.py
+++ practices/practice2/5_1_ham_dist.py
@@ -7,7 +7,7 @@ def ham_dist(num1: int, num2: int) -> int:
     if len(num1) > len(num2):
         num2 = format(int(num2, 2), f'#0{len(num1)}b')
     elif len(num1) < len(num2):
-        num1 = format(int(num1, 2), f'#0{len(num1)}b')
+        num1 = format(int(num1, 2), f'#0{len(num2)}b')
 
     for i in range(len(num1) - 1, -1, -1):
         if num1[i] != num2[i]:
